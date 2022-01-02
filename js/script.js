0886b2712237de85e156b2b32fffcb03ca52d513
verowangari <verowangari34@gmail.com>
2022-01-02 21:40:57 +0300

    show paragraph on what we do section upon click

---
+++ js/script.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+    
+    $(".card1").click(function(){
+        $(".imagehide1").slideToggle(1000);
+        $(".display1").slideToggle(2000);
+      });
+      $(".card2").click(function(){
+        $(".imagehide2").slideToggle(1000);
+        $(".display2").slideToggle(2000);
+      });
+      $(".card3").click(function(){
+        $(".imagehide3").slideToggle(1000);
+        $(".display3").slideToggle(2000);
+      });
+  });
