2327eec659f683eb2536fbba9fd937369fdc5dc0
verowangari <verowangari34@gmail.com>
2022-01-03 11:22:30 +0300

    add form functionality

--- js/script.js
+++ js/script.js
@@ -20,3 +20,14 @@ jQuery(document).ready(function() {
   function(){
     $(this).animate({opacity:'0'});
   })
+  $('.submit').click(function () {
+    var Name = $('#mce-FNAME').val();
+    var Email = $('#mce-EMAIL').val();
+    var Message = $('#mce-MESSAGE');
+    // var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
+    if (Name == '' || Email == '' || Message == '') {
+        alert('Please make sure you have filled in the form correctly!');
+    } else {
+        alert(' Hi, ' + ' We have received your message. Thank you for contacting us.');
+    }
+});
