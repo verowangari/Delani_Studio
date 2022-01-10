db09a8997854e59a60116d59c81b68a3bda1b39a
verowangari <verowangari34@gmail.com>
2022-01-10 11:54:09 +0300

    odify alert message

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ jQuery(document).ready(function() {
     var Name = $('#mce-FNAME').val();
     var Email = $('#mce-EMAIL').val();
     var Message = $('#mce-MESSAGE');
-    // var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
+    
     if (Name == '' || Email == '' || Message == '') {
         alert('Please make sure you have filled in the form correctly!');
     } else {
